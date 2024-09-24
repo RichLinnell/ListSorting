ab42a2afe74cc149d42d4ff4771baa9efd230f53
Rich Linnell <richlinnell@Richs-MacBook-Pro.local>
2024-09-24 17:13:04 +0100

    Next stage

--- Program.cs
+++ Program.cs
@@ -1,14 +1,22 @@
 ﻿string[] pallets = { "B14", "A11", "B12", "A13" };
+Console.WriteLine("");
 
-Console.WriteLine("Sorted...");
-Array.Sort(pallets);
+Console.WriteLine($"Before: {pallets[0]}");
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"After: {pallets[0]}");
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
 foreach (var pallet in pallets)
 {
     Console.WriteLine($"-- {pallet}");
 }
 Console.WriteLine("");
-Console.WriteLine("Reversed...");
-Array.Reverse(pallets);
+
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+pallets[4] = "C01";
+pallets[5] = "C02";
+
 foreach (var pallet in pallets)
 {
     Console.WriteLine($"-- {pallet}");
